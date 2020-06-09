6bbbae0ec9cd335a9e7ac338833d8655b944b9f8
NaomiBerce <57135448+NaomiBerce@users.noreply.github.com>
2020-06-09 23:27:19 +0300

    Add files via upload
    NrMare-OOP

---
+++ NumarMare.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text;
+namespace OOP_NumarMare
+{
+    public class NumarMare
+    {
+        private int[] NrMare;
+        private int length;
+
+        public NumarMare()
+        {
+
+        }
+
+        public NumarMare(string nr)
+        {
+            this.length = nr.Length;
+            this.NrMare = new int[nr.Length];
+            for (int i = nr.Length - 1; i >= 0; i--)
+            {
+                NrMare[nr.Length - 1 - i] = int.Parse((nr[i]).ToString());
+            }
+        }
+
+        public NumarMare(int length)
+        {
+            this.length = length;
+            this.NrMare = new int[length];
+        }
+
+        public static NumarMare operator +(NumarMare a, NumarMare b)
+        {
+            int min = Math.Min(a.NrMare.Length, b.NrMare.Length);
+            int max = Math.Max(a.NrMare.Length, b.NrMare.Length);
+            NumarMare c = new NumarMare(max + 1);
+
+            int k = 0;
+            for (int i = 0; i < min; i++)
+            {
+                c.NrMare[i] = a.NrMare[i] + b.NrMare[i] + k;
+
+                if (c.NrMare[i] >= 10)
+                {
+                    k = 1;
+                    c.NrMare[i] -= 10;
+                }
+                else
+                {
+                    k = 0;
+                }
+            }
+            NumarMare d = new NumarMare();
+            if (a.NrMare.Length > b.NrMare.Length)
+            {
+                d = a;
+            }
+            else
+            {
+                d = b;
+            }
+
+            for (int i = min; i < max; i++)
+            {
+                c.NrMare[i] = d.NrMare[i] + k;
+
+                if (c.NrMare[i] >= 10)
+                {
+                    k = 1;
+                    c.NrMare[i] -= 10;
+                }
+                else
+                {
+                    k = 0;
+                }
+            }
+
+            if (k == 1)
+            {
+                c.NrMare[max] = 1;
+            }
+
+            return c.RemoveZeros();
+        }
+
+        public NumarMare GetFactorial(int n)
+        {
+            NumarMare currentNumber = new NumarMare("1");
+            NumarMare factorial = new NumarMare("1");
+
+            for (int i = 0; i < n; i++)
+            {
+                factorial *= currentNumber;
+                currentNumber++;
+            }
+
+            return factorial;
+        }
+
+        public static NumarMare operator ++(NumarMare a)
+        {
+            return a + new NumarMare("1");
+        }
+
+        public static NumarMare operator *(NumarMare a, NumarMare b)
+        {
+            NumarMare c = new NumarMare(a.NrMare.Length + b.NrMare.Length);
+
+            NumarMare[] intermediar = new NumarMare[b.NrMare.Length];
+
+            for (int i = 0; i < b.length; i++)
+            {
+                NumarMare d = new NumarMare(a.NrMare.Length + b.NrMare.Length); ;
+                int produs, rest, cat = 0;
+
+                for (int j = 0; j < a.length; j++)
+                {
+
+                    produs = b.NrMare[i] * a.NrMare[j] + cat;
+                    rest = produs % 10;
+                    cat = produs / 10;
+
+                    d.NrMare[j + i] = rest;
+                }
+
+                if (cat > 0)
+                {
+                    d.NrMare[a.length + i] = cat;
+                }
+                intermediar[i] = d;
+            }
+
+            for (int i = 0; i < intermediar.Length; i++)
+            {
+                c = c + intermediar[i];
+            }
+
+            return c.RemoveZeros();
+        }
+
+        public NumarMare RemoveZeros()
+        {
+            int ultima_pozitie = -1;
+            for (int i = 0; i < this.NrMare.Length; i++)
+            {
+                if (this.NrMare[i] == 0)
+                {
+                    if (ultima_pozitie < 0)
+                    {
+                        ultima_pozitie = i;
+                    }
+                }
+                else
+                {
+                    ultima_pozitie = -1;
+                }
+            }
+            if (ultima_pozitie > 0)
+            {
+                NumarMare a = new NumarMare(ultima_pozitie);
+                for (int i = 0; i < a.NrMare.Length; i++)
+                {
+                    a.NrMare[i] = this.NrMare[i];
+                }
+                return a;
+            }
+            return this;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < this.NrMare.Length; i++)
+            {
+                sb.Append(this.NrMare[this.NrMare.Length - 1 - i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_NumarMare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clasa NumarMare");
+            NumarMare n1 = new NumarMare("1204");
+            NumarMare n2 = new NumarMare("202");
+            Console.WriteLine();
+
+            NumarMare n3 = n1 + n2;
+            Console.WriteLine($"{n1} + {n2} = {n3}");
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        private static NumarMare GetFibonacciNumber(int n)
+        {
+            NumarMare F0 = new NumarMare("0");
+            NumarMare F1 = new NumarMare("1");
+            NumarMare Fn = new NumarMare("0");
+
+            for (int i = 2; i <= n; i++)
+            {
+                Fn = F0 + F1;
+                F0 = F1;
+                F1 = Fn;
+            }
+
+            return Fn;
+        }
+    }
+}
